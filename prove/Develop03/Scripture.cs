339422fb78f05c66e9c5a7dd5ba05301a29ab95e
Aniekan Winner Anietie <winneraaniekan@gmail.com>
2024-07-03 11:26:44 +0100

    Refactor Word, Reference, and ScriptureLibrary classes to remove use of namespace

--- prove/Develop03/Scripture.cs
+++ prove/Develop03/Scripture.cs
@@ -2,55 +2,52 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 
-namespace ScriptureMemorizer
+public class Scripture
 {
-    public class Scripture
+    private Reference _reference;
+    private List<Word> _words;
+    private Random _random;
+
+    public Scripture(Reference reference, string text)
     {
-        private Reference _reference;
-        private List<Word> _words;
-        private Random _random;
+        _reference = reference;
+        _words = text.Split(' ').Select(word => new Word(word)).ToList();
+        _random = new Random();
+    }
 
-        public Scripture(Reference reference, string text)
+    public void HideRandomWords(int numberToHide)
+    {
+        var wordsToHide = _words.Where(word => !word.IsHidden()).ToList();
+        if (wordsToHide.Count == 0) return;
+
+        for (int i = 0; i < numberToHide && wordsToHide.Count > 0; i++)
         {
-            _reference = reference;
-            _words = text.Split(' ').Select(word => new Word(word)).ToList();
-            _random = new Random();
+            int index = _random.Next(wordsToHide.Count);
+            wordsToHide[index].Hide();
+            wordsToHide.RemoveAt(index);
         }
+    }
 
-        public void HideRandomWords(int numberToHide)
+    public string GetDisplayText()
+    {
+        return $"{_reference.GetDisplayText()}\n" + string.Join(" ", _words.Select(word => word.GetDisplayText()));
+    }
+
+    public string GetReferenceText()
+    {
+        return _reference.GetDisplayText();
+    }
+
+    public bool IsCompletelyHidden()
+    {
+        return _words.All(word => word.IsHidden());
+    }
+
+    public void ResetWords()
+    {
+        foreach (var word in _words)
         {
-            var wordsToHide = _words.Where(word => !word.IsHidden()).ToList();
-            if (wordsToHide.Count == 0) return;
-
-            for (int i = 0; i < numberToHide && wordsToHide.Count > 0; i++)
-            {
-                int index = _random.Next(wordsToHide.Count);
-                wordsToHide[index].Hide();
-                wordsToHide.RemoveAt(index);
-            }
-        }
-
-        public string GetDisplayText()
-        {
-            return $"{_reference.GetDisplayText()}\n" + string.Join(" ", _words.Select(word => word.GetDisplayText()));
-        }
-
-        public string GetReferenceText()
-        {
-            return _reference.GetDisplayText();
-        }
-
-        public bool IsCompletelyHidden()
-        {
-            return _words.All(word => word.IsHidden());
-        }
-
-        public void ResetWords()
-        {
-            foreach (var word in _words)
-            {
-                word.Show();
-            }
+            word.Show();
         }
     }
 }
--- prove/Develop03/ScriptureLibrary.cs
+++ prove/Develop03/ScriptureLibrary.cs
@@ -2,55 +2,52 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 
-namespace ScriptureMemorizer
+public class ScriptureLibrary
 {
-    public class ScriptureLibrary
+    private List<Scripture> _scriptures;
+    private Random _random;
+
+    public ScriptureLibrary()
     {
-        private List<Scripture> _scriptures;
-        private Random _random;
+        _scriptures = new List<Scripture>();
+        _random = new Random();
+    }
 
-        public ScriptureLibrary()
+    public void AddScripture(Reference reference, string text)
+    {
+        _scriptures.Add(new Scripture(reference, text));
+    }
+
+    public Scripture GetRandomScripture()
+    {
+        int index = _random.Next(_scriptures.Count);
+        return _scriptures[index];
+    }
+
+    public Scripture GetScriptureByIndex(int index)
+    {
+        if (index < 0 || index >= _scriptures.Count)
         {
-            _scriptures = new List<Scripture>();
-            _random = new Random();
+            throw new ArgumentOutOfRangeException(nameof(index), "Index is out of range.");
         }
+        return _scriptures[index];
+    }
 
-        public void AddScripture(Reference reference, string text)
-        {
-            _scriptures.Add(new Scripture(reference, text));
-        }
+    public int GetScriptureCount()
+    {
+        return _scriptures.Count;
+    }
 
-        public Scripture GetRandomScripture()
-        {
-            int index = _random.Next(_scriptures.Count);
-            return _scriptures[index];
-        }
+    public bool AllScripturesMemorized()
+    {
+        return _scriptures.All(scripture => scripture.IsCompletelyHidden());
+    }
 
-        public Scripture GetScriptureByIndex(int index)
+    public void ResetMemorization()
+    {
+        foreach (var scripture in _scriptures)
         {
-            if (index < 0 || index >= _scriptures.Count)
-            {
-                throw new ArgumentOutOfRangeException(nameof(index), "Index is out of range.");
-            }
-            return _scriptures[index];
-        }
-
-        public int GetScriptureCount()
-        {
-            return _scriptures.Count;
-        }
-
-        public bool AllScripturesMemorized()
-        {
-            return _scriptures.All(scripture => scripture.IsCompletelyHidden());
-        }
-
-        public void ResetMemorization()
-        {
-            foreach (var scripture in _scriptures)
-            {
-                scripture.ResetWords();
-            }
+            scripture.ResetWords();
         }
     }
 }
