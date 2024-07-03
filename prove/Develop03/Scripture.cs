c21b8a8d4e306fe59165b7a3c53668ac81424810
Aniekan Winner Anietie <winneraaniekan@gmail.com>
2024-07-03 11:08:59 +0100

    feat: Add Scripture class for managing scripture text and references

---
+++ prove/Develop03/Scripture.cs
@@ -0,0 +1,56 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace ScriptureMemorizer
+{
+    public class Scripture
+    {
+        private Reference _reference;
+        private List<Word> _words;
+        private Random _random;
+
+        public Scripture(Reference reference, string text)
+        {
+            _reference = reference;
+            _words = text.Split(' ').Select(word => new Word(word)).ToList();
+            _random = new Random();
+        }
+
+        public void HideRandomWords(int numberToHide)
+        {
+            var wordsToHide = _words.Where(word => !word.IsHidden()).ToList();
+            if (wordsToHide.Count == 0) return;
+
+            for (int i = 0; i < numberToHide && wordsToHide.Count > 0; i++)
+            {
+                int index = _random.Next(wordsToHide.Count);
+                wordsToHide[index].Hide();
+                wordsToHide.RemoveAt(index);
+            }
+        }
+
+        public string GetDisplayText()
+        {
+            return $"{_reference.GetDisplayText()}\n" + string.Join(" ", _words.Select(word => word.GetDisplayText()));
+        }
+
+        public string GetReferenceText()
+        {
+            return _reference.GetDisplayText();
+        }
+
+        public bool IsCompletelyHidden()
+        {
+            return _words.All(word => word.IsHidden());
+        }
+
+        public void ResetWords()
+        {
+            foreach (var word in _words)
+            {
+                word.Show();
+            }
+        }
+    }
+}
